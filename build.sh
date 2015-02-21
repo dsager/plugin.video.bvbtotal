8ce01a22251b47753cdc07e3edfcc6c7dc9caa6b
Daniel Sager <daniel@sager1.de>
2015-02-21 18:43:59 +0100

    build script

--- build.sh
+++ build.sh
@@ -1,9 +1,13 @@
 #!/bin/sh
-DEST="dist/plugin.video.bvbtotal-0.1.0"
+VERSION="0.1.0"
+NAME="plugin.video.bvbtotal-${VERSION}"
+DEST="dist/${NAME}"
 
-mkdir -p ${DEST}
-cp ./addon.xml ${DEST}/addon.xml
-cp ./addon.py ${DEST}/addon.py
-cp ./icon.png ${DEST}/icon.png
-cp ./changelog.txt ${DEST}/changelog.txt
-cp ./LICENSE.txt ${DEST}/LICENSE.txt
+mkdir -p "${DEST}"
+cp ./addon.xml "${DEST}/addon.xml"
+cp ./addon.py "${DEST}/addon.py"
+cp ./icon.png "${DEST}/icon.png"
+cp ./changelog.txt "${DEST}/changelog.txt"
+cp ./LICENSE.txt "${DEST}/LICENSE.txt"
+cd ./dist
+zip -r "${NAME}.zip" "${NAME}"
